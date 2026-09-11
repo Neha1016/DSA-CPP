4f19326eb7952d8d8f579a0f1fc687709f7c19cb
Neha1016 <nehachouhan1016@gmail.com>
2026-09-11 22:28:25 +0530

    ADD BASICS

---
+++ practice.cpp
@@ -0,0 +1,77 @@
+// #include <iostream>
+// using namespace std;
+// int main () {
+
+
+    // // check prime number 
+    // int n = 7;
+
+    // for (int i = 2; i < n; i++){
+    //     if(n % i == 0){
+    //         cout << " It is not prime ";
+    //         return 0;
+    //     }
+        
+    // cout << "It is Prime ";
+        
+
+    
+        //check it is plaindorme or not 
+    //int n = 123;
+    // int n = 121;
+    // int rev = 0;
+    // int original = n;
+
+    // while (n > 0){
+
+    //     int digit = n % 10;
+    //     rev = rev * 10 + digit;
+    //     n = n / 10;
+    // }
+
+    // if (original == rev){
+    //     cout << "It is a palindrome : " << original;
+    // }
+    // else{
+    //     cout << "It is not a palindrome :" << original;
+    // }
+
+        // count a number
+        // int n = 12345947;
+        // int count = 0;
+
+        // while( n > 0) {
+        //     count ++;
+        //     n = n / 10;
+        // }
+
+        // cout << count  ;
+
+        // reverse a number
+        // int n = 45678;
+
+        // int reverse = 0;
+
+        // while (n > 0){
+        //     int digit = n % 10;
+        //     reverse = reverse * 10 + digit;
+        //     n = n / 10;
+        // }
+
+        // cout << reverse;
+
+        // factorial of number
+        //     int n = 5;
+
+        //     int fact = 1;
+
+        //     for(int i = 1; i <= n; i++){
+        //         fact *= i;
+        //     }
+
+        //     cout << fact << " ";
+
+//         return 0;
+
+//     }
+// }
