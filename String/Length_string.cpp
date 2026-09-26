ae6dd36a523b775525a6fd29b85d471da055cbc9
Neha1016 <nehachouhan1016@gmail.com>
2026-09-26 21:55:00 +0530

    ADD STRINGS

---
+++ String/Length_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    string str;
+
+    cout << "Enter a string : ";
+    cin >> str;
+
+    cout << "Length = " << str.length();
+
+    return 0;
+}
